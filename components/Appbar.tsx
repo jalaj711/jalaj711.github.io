dadeb01a920f1cc62d6bb97e83b4b268d70e89c8
Jalaj Kumar <96870071+jalaj-07@users.noreply.github.com>
2022-01-13 11:02:43 +0530

    make appbar links button-like
    Signed-off-by: Jalaj Kumar <96870071+jalaj-07@users.noreply.github.com>

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -77,6 +77,7 @@ export default function ApplicationBar() {
                   onClick={() => router.push('#')}
                   component="a"
                   href="#"
+                  variant="button"
                 >
                   {elem}
                 </Typography>
