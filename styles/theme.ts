13f8041317a9768d54c6903625c4714f015e7d7d
Jalaj Kumar <96870071+jalaj-07@users.noreply.github.com>
2022-01-24 11:59:30 +0530

    add secondary color to theme
    Signed-off-by: Jalaj Kumar <96870071+jalaj-07@users.noreply.github.com>

--- styles/theme.ts
+++ styles/theme.ts
@@ -6,6 +6,9 @@ const theme = createTheme({
         primary: {
             main: "#c0ff03",
         },
+        secondary: {
+            main: "#00d0ff"
+        },
         background: {
             default: "rgb(0,30,50)",
             paper: "rgb(10, 25, 41)"
