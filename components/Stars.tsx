8c932ef4b3abc0928d7212033d3922fadd382b7c
Jalaj Kumar <96870071+jalaj-07@users.noreply.github.com>
2022-02-04 11:12:16 +0530

    use `styled` API for Stars
    Signed-off-by: Jalaj Kumar <96870071+jalaj-07@users.noreply.github.com>

--- components/Stars.tsx
+++ components/Stars.tsx
@@ -1,30 +1,36 @@
-export default function Star(props: {count: number}){
-    const starStyle = {
-        width: "5px",
-        height: "5px",
-        borderRadius: "50%",
-        display: "block",
-        background: "ghostwhite",
-	zIndex: -10,
-        boxShadow: "ghostwhite 0px 0px 20px 5px, ghostwhite 0 0 10px 1px"
-      }
-    var arr: Array<Array<number>> = []
-    for(var i=0;i<props.count;i++){
-        arr.push([Math.round(Math.random() * 90 + 5), Math.round(Math.random() * 90 + 5)])
-    }
-    return (
-        <>
-        {
-        arr.map((elem, index) => <span
-            style={{
-              ...starStyle,
-              position: "absolute",
-              top: elem[0] + "%",
-              left: elem[1] + "%",
-            }}
-            key={index}
-          />)
-        }
-        </>
-        )
+import { styled } from "@mui/material";
+
+export const Star = styled("span")(() => ({
+  width: "5px",
+  height: "5px",
+  borderRadius: "50%",
+  display: "block",
+  background: "ghostwhite",
+  zIndex: -10,
+  boxShadow: "ghostwhite 0px 0px 20px 5px, ghostwhite 0 0 10px 1px",
+}));
+
+export default function RandomStars(props: { count: number }) {
+  var arr: Array<Array<number>> = [];
+  for (var i = 0; i < props.count; i++) {
+    arr.push([
+      Math.round(Math.random() * 90 + 5),
+      Math.round(Math.random() * 90 + 5),
+    ]);
+  }
+  return (
+    <>
+      {arr.map((elem, index) => (
+        <Star
+          style={{
+            position: "absolute",
+            top: elem[0] + "%",
+            left: elem[1] + "%",
+          }}
+          key={index}
+        />
+      ))}
+    </>
+  );
 }
+
