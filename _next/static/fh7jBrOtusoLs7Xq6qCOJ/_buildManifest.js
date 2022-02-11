b9ed9e93aded392bfbef0a3740ea7a1720d9cd09
jalaj-07 <96870071+jalaj-07@users.noreply.github.com>
2022-02-11 11:18:47 +0000

    Update deploy with master

---
+++ _next/static/fh7jBrOtusoLs7Xq6qCOJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/881-f3a23d8415684183.js","static/chunks/pages/index-60e7e1fe516e9c5b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
